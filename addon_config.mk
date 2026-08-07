b3bf77af04bcfead6c0a913949777bee84507a98
Bruno Herfst <bruno.herfst@gmail.com>
2026-08-07 22:41:46 +1000

    Repoint ofKitty URLs to ofxyz

--- addon_config.mk
+++ addon_config.mk
@@ -3,7 +3,7 @@ meta:
 	ADDON_DESCRIPTION = Optional ofKitty integration: Reusable ImGui windows and panels for ofxPlotter.
 	ADDON_AUTHOR = ofKitty
 	ADDON_TAGS = "addon" "plotter" "imgui" "ofkitty"
-	ADDON_URL = https://github.com/ofkitty/ofxPlotterKit
+	ADDON_URL = https://github.com/ofxyz/ofxPlotterKit
 
 common:
 	# ofxPixelPlotter: PlotterEffectGraphECS / EffectPhase used by GcodeGeneratorPanel
